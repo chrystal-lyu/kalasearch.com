379eabfd17b527104fa17916ec9a631d457f23d0
Chrystal Lyu <chrystal.lyu@gmail.com>
2020-03-30 17:01:55 -0700

    blog page renders list of post titles from files

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,57 @@
 import React from "react"
+import styled from 'styled-components';
+import { graphql } from 'gatsby'
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Blog = () => (
-  <Layout>
-    <SEO title="Blog" />
-    <h1>Hi from Blog page</h1>
-    <p>Welcome to Blog Page</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+  display: flex;
+  flex-direction: column;
+`
+
+const Blog = ({ data }) => {
+  const { edges } = data.allMarkdownRemark;
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <Wrapper>
+        {
+          edges.map((edge) => {
+            const { frontmatter } = edge.node;
+            return (
+              <h1 key = {frontmatter.path}>
+                {frontmatter.title}
+              </h1>
+            )
+          })
+        }
+        <Link to="/">Go back to the homepage</Link>
+      </Wrapper>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query BlogListQuery {
+    allMarkdownRemark (
+      sort: {order: DESC, fields: [frontmatter___date]}
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    }    
+  }
+`
 
 export default Blog;
